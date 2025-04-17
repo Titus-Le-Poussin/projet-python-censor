4c961f4bc99e6a8370c3029fdc03552d0d712885
Timothée <timotheeithier@gmail.com>
2025-04-17 16:43:19 +0200

    Sauvegarde du code après récupération

--- cencured_File.py
+++ cencured_File.py
@@ -1,17 +1,46 @@
 import random
-List_debut = {"Espece","Sac","Bougre","Tête","Tronche","Crotte"}
-List_adjectif = {"Vieux","Gros","Fou","Lumineux","spongieux","mou","radioactif","sombre","malefique","gélatineux","gluant","Malodorant"}
-List_Nom_Rigolos = {"Lama","Nouille","Champignon","Cornichon","Limace","Chausette"}
-List_mot_entre = {"de","à","et","le","la","les","des","du"}
+import pandas as pd
 
-debut = random.choice(list(List_debut))
-Adjectif = random.choice(list(List_adjectif))
-Nom_Rigolos = random.choice(list(List_Nom_Rigolos))
-List_mot_entre = random.choice(list(List_mot_entre))
+# Chemin vers le fichier Excel
+fichier_excel = "C:\\Users\\Utilisateur\\Documents\\informatique\\RandomListPythonCencorFile.xlsx"
+
+# Lire les différentes feuilles du fichier Excel
+feuille1 = pd.read_excel(fichier_excel, sheet_name="Feuille1")
+feuille2 = pd.read_excel(fichier_excel, sheet_name="Feuille2")
+feuille3 = pd.read_excel(fichier_excel, sheet_name="Feuille3")
+feuille4 = pd.read_excel(fichier_excel, sheet_name="Feuille4")
+
+# Extraire les colonnes ou données nécessaires
+List_debut = feuille1["mot"].dropna().tolist()  
+List_adjectif = feuille2["ADJ"].dropna().tolist()  
+List_Nom_Rigolos = feuille3["Nom"].dropna().tolist()  
+List_mot_entre = feuille4["Determinant"].dropna().tolist()
+Genre_ADJ = feuille2["Genre"].dropna().tolist()
+Genre_Nom = feuille3["Genre"].dropna().tolist()
+Voyelle_ADJ = feuille2["Voyelle"].dropna().tolist()
+Voyelle_Nom = feuille3["Voyelle"].dropna().tolist()
+
+# Générer des phrases aléatoires
+for i in range(20):
+    Nom_Rigolos = random.choice(List_Nom_Rigolos)
+    genre_nom_index = List_Nom_Rigolos.index(Nom_Rigolos)
+    genre_nom = Genre_Nom[genre_nom_index]
+
+    if Voyelle_Nom[genre_nom_index] == 1:
+        Nom_Rigolos = "d'" + Nom_Rigolos
+    else:
+        Nom_Rigolos = "de " + Nom_Rigolos
+    
 
 
-for i in range(2):
-    Phrase = f"{debut} de {Nom_Rigolos} {Adjectif}"
+    if genre_nom == "H":
+        adj_filtrés = [adj for adj, genre in zip(List_adjectif, Genre_ADJ) if genre == "H"]
+    else:
+        adj_filtrés = [adj for adj, genre in zip(List_adjectif, Genre_ADJ) if genre == "F"]
+        
+    Adjectif = random.choice(adj_filtrés)
+    debut = random.choice(List_debut)
+
+
+    Phrase = f"{debut} {Nom_Rigolos} {Adjectif}"
     print(Phrase)
-
-
