dcd9d2b7291c288d9766a8e63f26c5778584818c
Timothée <timotheeithier@gmail.com>
2025-04-17 15:50:12 +0200

    Sauvegarde de la version actuelle

--- cencured_File.py
+++ cencured_File.py
@@ -1,9 +1,17 @@
 import random
-debut = {"Espèce","Sac","Bougre","Tête","Morceau","Tronche","Bete","Crotte","Vieux","Petit"}
-adjectif = {"Vieux","Gros","Fou","Lumineux","spongieux","mou","radioactif","sombre","maléfique","gélatineux","gluant","Malodorant"}
-Nom_Rigolos = {"Lama","Nouille","Champignon","Cornichon","Limace","Chausette"}
-mot_entre = {"de","à","et","le","la","les","des","du"}
+List_debut = {"Espece","Sac","Bougre","Tête","Tronche","Crotte"}
+List_adjectif = {"Vieux","Gros","Fou","Lumineux","spongieux","mou","radioactif","sombre","malefique","gélatineux","gluant","Malodorant"}
+List_Nom_Rigolos = {"Lama","Nouille","Champignon","Cornichon","Limace","Chausette"}
+List_mot_entre = {"de","à","et","le","la","les","des","du"}
 
-Phrase = f"{random.choice(list(debut))} {random.choice(list(mot_entre))} {random.choice(list(adjectif))} {random.choice(list(Nom_Rigolos))}"
-print(Phrase)
-print("probleme")
+debut = random.choice(list(List_debut))
+Adjectif = random.choice(list(List_adjectif))
+Nom_Rigolos = random.choice(list(List_Nom_Rigolos))
+List_mot_entre = random.choice(list(List_mot_entre))
+
+
+for i in range(2):
+    Phrase = f"{debut} de {Nom_Rigolos} {Adjectif}"
+    print(Phrase)
+
+
